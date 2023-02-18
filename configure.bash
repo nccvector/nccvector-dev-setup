8c78445259bfb6e0c52833cf72f59706fc19172c
nccvector <fzn1020@gmail.com>
2023-02-18 18:45:41 +0900

    configure.bash retired

--- configure.bash
+++ configure.bash
@@ -5,10 +5,10 @@ XDG_CONFIG_HOME=~/.config
 # Getting current script directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-source installEssentials.bash
-source configureConfigs.bash
-source configureNeovim.bash
-source configureTerminator.bash
-source configurePythonPip.bash
-source configureCustomCommands.bash
+# source installEssentials.bash
+# source configureConfigs.bash
+# source configureNeovim.bash
+# source configureTerminator.bash
+# source configurePythonPip.bash
+# source configureCustomCommands.bash
 
