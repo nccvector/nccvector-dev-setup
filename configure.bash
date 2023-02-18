dcf99710de21e443712fbde401c3f418e3abadb3
nccvector <fzn1020@gmail.com>
2023-02-18 18:44:38 +0900

    refactor

--- configure.bash
+++ configure.bash
@@ -5,19 +5,10 @@ XDG_CONFIG_HOME=~/.config
 # Getting current script directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Install essentials
-sudo apt install git cmake make -y
-sudo apt install fonts-firacode -y	# Needed for terminator config
-sudo apt install brightnessctl -y # Needed for qtile brightness widget
-
-# Configure the configs
+source installEssentials.bash
 source configureConfigs.bash
-
 source configureNeovim.bash
-
 source configureTerminator.bash
-
 source configurePythonPip.bash
-
 source configureCustomCommands.bash
 
---
+++ installEssentials.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Install essentials
+sudo apt install git cmake make -y
+sudo apt install fonts-firacode -y	# Needed for terminator config
+
