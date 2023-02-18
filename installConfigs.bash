70d340ca12f73ba03f669d4aef0daada7d1c71d0
nccvector <fzn1020@gmail.com>
2023-02-18 19:22:45 +0900

    attach remote to ~/.config

--- installConfigs.bash
+++ installConfigs.bash
@@ -1,22 +1,10 @@
-
 #!/usr/bin/env bash
 
 XDG_CONFIG_HOME=~/.config
 
-# Getting current script directory
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# Moving into .config and attaching config remote
+cd $XDG_CONFIG_HOME
 
-# Cloning
-if [ ! -d "$SCRIPT_DIR/configs" ]; then
-	# Cloning neovim
-	git clone https://github.com/nccvector/configs.git
-fi
-
-# Pull the latest configs
-cd configs
-git pull
-cd ..
-
-# Copying the configs to XDG_CONFIG_HOME
-
-yes | cp -rf $SCRIPT_DIR/configs/* $XDG_CONFIG_HOME/
+git init
+git remote add origin git@github.com:nccvector/configs.git
+git checkout -f main
